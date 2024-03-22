df889c1672c71d23816b847c207313de0dea621e
Hwang <yugecosecant@foxmail.com>
2024-03-22 22:16:19 +0800

    #优化更新，兼容gradle8.0; 优化gradle写法

---
+++ example/android/app/src/main/java/fr/g123k/flutterappbadger/flutterappbadgerexample/MainActivity.java
@@ -0,0 +1,17 @@
+package fr.g123k.flutterappbadger.flutterappbadgerexample;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+
+    // @Override
+    // public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    //     super.configureFlutterEngine(flutterEngine);
+    //     GeneratedPluginRegistrant.registerWith(flutterEngine);
+    // }
+
+}
